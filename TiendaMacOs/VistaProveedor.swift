c0bef855374da791aae37e10f89cece9de5255d6
kleber <klever>
2026-03-26 09:36:47 -0500

    Correccion de la vista y del view model

---
+++ TiendaMacOs/VistaProveedor.swift
@@ -0,0 +1,18 @@
+//
+//  VistaProveedor.swift
+//  TiendaMacOs
+//
+//  Created by kleber oswaldo muy landi on 25/3/26.
+//
+
+import SwiftUI
+
+struct VistaProveedor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaProveedor()
+}
